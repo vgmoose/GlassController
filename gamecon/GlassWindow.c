9d4790ba1f520bbc11bb173ae8b46ca7d758e9e0
vgmoose <me@vgmoose.com>
2018-01-14 00:35:59 -0500

    initial commit, types 's' every 2 seconds, listens for touchpad events

---
+++ gamecon/GlassWindow.c
@@ -0,0 +1,8 @@
+//
+//  GlassWindow.c
+//  gamecon
+//
+//  Created by Ricky Ayoub on 1/12/18.
+//
+
+#include "GlassWindow.h"
---
+++ gamecon/GlassWindow.h
@@ -0,0 +1,13 @@
+//
+//  GlassWindow.h
+//  gamecon
+//
+//  Created by Ricky Ayoub on 1/12/18.
+//
+
+#ifndef GlassWindow_h
+#define GlassWindow_h
+
+#include <stdio.h>
+
+#endif /* GlassWindow_h */
